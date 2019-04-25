9b14f50919432cb2e20944a2642a0a79f7b5616e
Vui Nguyen <vui.nguyen@flatironschool.com>
2019-04-25 22:37:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName;
+const leastFavoriteCustomer = 'harry';
+
+function  upperCaseCustomerName() {
+  customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  var bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(customer) {
+  bestCustomer = customer;
+}
+
+function changeLeastFavoriteCustomer(customer) {
+  leastFavoriteCustomer = customer;
+}
